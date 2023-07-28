1c34bf477d621be51b0be2036d1a2618fd46a537
Chalagalla Vinitha <91236364+Pecclg@users.noreply.github.com>
2023-07-28 10:14:05 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,10 @@
+class Addition
+{
+  public static void main(String args[])
+  {
+    int a=12,b=7;
+    int sum=0;
+    sum=a+b;
+    System.out.println("Addition of "+a+" and "+b+" is: "+sum);
+  }
+}
